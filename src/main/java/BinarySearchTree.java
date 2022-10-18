5d1624301aeb76e0a7024245af4a380a8b1c47b8
AntoineSoldati <90266863+AntoineSoldati@users.noreply.github.com>
2022-10-18 09:39:46 -0400

    Completed binarySearchTree implementation

--- src/main/java/BinarySearchTree.java
+++ src/main/java/BinarySearchTree.java
@@ -5,8 +5,19 @@ public class BinarySearchTree<T extends Comparable<T>> extends BinaryTree<T> imp
     }
 
     protected BinaryNode<T> add(T value, BinaryNode<T> curNode) {
-        //TODO
-        return null;
+        // Insérer si position courante est une feuille
+        if( curNode == null )
+            return new BinaryNode<T>( value );
+        // Sinon trouver position
+        int compareResult = value.compareTo( curNode.value );
+        if(compareResult < 0 )
+            curNode.left = add( value, curNode.left );
+        else if( compareResult > 0 )
+            curNode.right = add( value, curNode.right );
+        else
+            ; // Rejeter en cas de doublon
+        return curNode;
+
     }
 
     @Override
@@ -25,12 +36,31 @@ public class BinarySearchTree<T extends Comparable<T>> extends BinaryTree<T> imp
     }
 
     protected BinaryNode<T> remove(T value, BinaryNode<T> curNode) {
-        //TODO
-        return null;
+        // Si x absent, resortir
+        if( curNode == null )
+            return curNode;
+        // Rechercher position de x
+        int compareResult = value.compareTo( curNode.value );
+        if( compareResult < 0 )
+            curNode.left = remove( value, curNode.left );
+        else if( compareResult > 0 )
+            curNode.right = remove( value, curNode.right );
+        // Une fois x trouvé
+        else if( curNode.left != null && curNode.right != null ) // Cas deux enfants
+        {
+            curNode.value = findMin( curNode.right ).value;
+            curNode.right = remove( curNode.value, curNode.right );
+        }
+        else // Cas trivial 
+            curNode = ( curNode.left != null ) ? curNode.left : curNode.right;
+        return curNode;
     }
 
     protected BinaryNode<T> findMin(BinaryNode<T> curNode){
-        //TODO
-        return null;
+        if( curNode == null )
+            return null;
+        else if( curNode.left == null )
+            return curNode;
+        return findMin( curNode.left );
     }
 }
