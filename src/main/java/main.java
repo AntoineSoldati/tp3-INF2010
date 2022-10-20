e6a378b7c469fb52c7778d5284f0ef8ccd8b3ae8
AntoineSoldati <90266863+AntoineSoldati@users.noreply.github.com>
2022-10-20 16:49:15 -0400

    Add search and BST insertion worst case stats generation of csv file for plotting

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,99 +1,121 @@
-import java.io.Console;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.ArrayList;
 
 
 public class main {
+
+    final static int MAX = 500;
+    final static int MIN = 0;
+    final static Timer TIMER = new Timer();
     public static void main(String[] args) throws IOException {
 
-        final int MAX = 500;
-        final int MIN = 0;
+        ArrayList<Integer> insertedValues = new ArrayList<Integer>();
 
-        Timer timer = new Timer();
         AvlTree<Integer> avl = new AvlTree<Integer>();
         BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>();
+         
+        ArrayList<String> csvBstInsertion = new ArrayList<>();
+        ArrayList<String> csvBstSearch = new ArrayList<>();
+        ArrayList<String> csvBstInsertionWorst = new ArrayList<>();
+        writeCsvHeaders(csvBstSearch);
+        writeCsvHeaders(csvBstInsertion);
+        writeCsvHeaders(csvBstInsertionWorst);
 
-        long[][] avlOperationStats = new long[100][2];
-        long[][] bstOperationStats = new long[100][2];
-        
-        //CSV file for graph 
-        ArrayList<String> csvBst = new ArrayList<>();
-        csvBst.add("Number Of Node");
-        csvBst.add("Number of operation");
-        csvBst.add("Execution Time");
+        ArrayList<String> csvAvlInsertion = new ArrayList<>();
+        ArrayList<String> csvAvlSearch = new ArrayList<>();
+        ArrayList<String> csvAvlInsertionWorst = new ArrayList<>();
+        writeCsvHeaders(csvAvlSearch);
+        writeCsvHeaders(csvAvlInsertion);
+        writeCsvHeaders(csvAvlInsertionWorst);
 
-        ArrayList<String> csvAvl = new ArrayList<>();
-        csvAvl.add("Number Of Node");
-        csvAvl.add("Number of operation");
-        csvAvl.add("Execution Time");
-
+        //Insertion
         for (Integer i = 0; i < 100; i++){
-            Integer nodeCounter = i + 1;
             //Méthode de trouver un nombre random trouvé sur : 
             //https://stackoverflow.com/questions/363681/how-do-i-generate-random-integers-within-a-specific-range-in-java
-            int value = ThreadLocalRandom.current().nextInt(MIN, MAX + 1);
-
-            timer.timerInit();
-            bst.counter.resetCounter();
-            bst.add(value);
-            Integer counter = bst.counter.getCounter();
-            Long time = timer.getTimerValue();
-            bstOperationStats[i][0] = counter;
-            bstOperationStats[i][1] = time;
-            
-            csvBst.add(nodeCounter.toString());
-            csvBst.add(counter.toString());
-            csvBst.add(Integer.valueOf(time.intValue()).toString()); //La conversion de long en int est valide puisque 
-                  
-            //les temps d'exécutions sont rapides et les valeurs petites
-            timer.timerInit();
-            avl.counter.resetCounter();
-            avl.add(value);
-            counter = avl.counter.getCounter();
-            time = timer.getTimerValue();
-            avlOperationStats[i][0] = counter;
-            avlOperationStats[i][1] = time;
-
-            csvAvl.add(nodeCounter.toString());
-            csvAvl.add(counter.toString());
-            csvAvl.add(Integer.valueOf(time.intValue()).toString()); //La conversion de long en int est valide puisque 
-                                                                    //les temps d'exécutions sont rapides et les valeurs petites
-
-            //System.out.println("\nBinary Search Tree number of operations : " + bstOperationStats[i][0] + " and time of execution : " + bstOperationStats[i][1] + " for " + (i + 1) + "'n insertion");
-            //System.out.println("AVL tree number of operations : " + avlOperationStats[i][0] + " and time of execution : " + avlOperationStats[i][1] + " for " + (i + 1) + "'n insertion");
+            Integer value = ThreadLocalRandom.current().nextInt(MIN, MAX + 1);
+            insertedValues.add(value);
+            computeInsertionStats(value, bst, csvBstInsertion, i);
+            computeInsertionStats(value, avl, csvAvlInsertion, i);
+            computeInsertionStats(i, bst, csvBstInsertionWorst, i);
+        }
+        
+        //Search
+        Integer index = 1;
+        for (Integer value : insertedValues) {
+            computeSearchStats(value, bst, csvBstSearch, index);
+            computeSearchStats(value, avl, csvAvlSearch, index++);
         }
 
-        File fileBst = new File("bstInsertion.txt");
-        fileBst.createNewFile();
+        //Write to file for plotting graphs
+        File fileBstInsertion = new File("bstInsertion.txt");
+        fileBstInsertion.createNewFile();
 
-        File fileAvl = new File("avlInsertion.txt");
-        fileAvl.createNewFile();
+        File fileAvlInsertion = new File("avlInsertion.txt");
+        fileAvlInsertion.createNewFile();
 
-        if (fileBst.exists() && fileAvl.exists()){ //Faire une fonction ou une classe
-            try(FileWriter filewriter = new FileWriter(fileBst);){
-                for (int i = 0; i < csvBst.size(); i++){
-                    filewriter.write(csvBst.get(i));
-                    filewriter.write(";");
-                }
-                filewriter.flush();
-                filewriter.close();
-            }
+        File fileBstSearch = new File("bstSearch.txt");
+        fileBstSearch.createNewFile();
 
-            try(FileWriter filewriter = new FileWriter(fileAvl);){
-                for (int i = 0; i < csvAvl.size(); i++){
-                    filewriter.write(csvAvl.get(i));
+        File fileAvlSearch = new File("avlSearch.txt");
+        fileAvlSearch.createNewFile();
+
+        File fileBstInsertionWorst = new File("bstInsertionWorst.txt");
+        fileBstInsertionWorst.createNewFile();
+
+        writeToFile(fileBstInsertion, csvBstInsertion);
+        writeToFile(fileAvlInsertion, csvAvlInsertion);
+        writeToFile(fileBstSearch, csvBstSearch);
+        writeToFile(fileAvlSearch, csvAvlSearch);
+        writeToFile(fileBstInsertionWorst, csvBstInsertionWorst);
+    }
+
+    private static void writeToFile(File file, ArrayList<String> csv) throws IOException{
+        if (file.exists()){
+            try(FileWriter filewriter = new FileWriter(file);){
+                for (int i = 0; i < csv.size(); i++){
+                    filewriter.write(csv.get(i));
                     filewriter.write(";");
                 }
                 filewriter.flush();
                 filewriter.close();
             }
         }
+    }
+
+    private static void computeInsertionStats(Integer insertedValue, BinarySearchTree<Integer> tree, ArrayList<String> csv, Integer index){
+        Integer nodeCounter = index + 1;
+        
+        TIMER.timerInit();
+        tree.counter.resetCounter();
+        tree.add(insertedValue);
+        Integer counter = tree.counter.getCounter();
+        Long time = TIMER.getTimerValue();
+        
+        csv.add(nodeCounter.toString());
+        csv.add(counter.toString());
+        csv.add(Integer.valueOf(time.intValue()).toString()); //La conversion de long en int est valide puisque les temps d'exécutions sont rapides et les valeurs petites
+    }
+
+    private static void computeSearchStats(Integer value, BinarySearchTree<Integer> tree, ArrayList<String> csv, Integer index){        
+        TIMER.timerInit();
+        tree.counter.resetCounter();
+        tree.contains(value);
+        Integer counter = tree.counter.getCounter();
+        Long time = TIMER.getTimerValue();
+        
+        csv.add(index.toString());
+        csv.add(counter.toString());
+        csv.add(Integer.valueOf(time.intValue()).toString()); //La conversion de long en int est valide puisque les temps d'exécutions sont rapides et les valeurs petites
+    }
+
+    private static ArrayList<String> writeCsvHeaders(ArrayList<String> csv){
+        csv.add("Number Of Node");
+        csv.add("Number of operation");
+        csv.add("Execution Time");
+
+        return csv;
     }
 }
