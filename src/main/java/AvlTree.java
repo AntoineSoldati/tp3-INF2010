0e4cc403c377d960470096bfc33f1ea0d46e83d2
Elouan Guyon <elouan.guyon@polymtl.ca>
2022-10-24 11:04:35 -0400

    Replaced old method signatures

--- src/main/java/AvlTree.java
+++ src/main/java/AvlTree.java
@@ -1,10 +1,18 @@
 public class AvlTree<T extends Comparable<T>> extends BinarySearchTree<T>{
     @Override
+    public void add(T value) {
+        this.root = add(value, this.root);
+    }
+
     protected BinaryNode<T> add(T value, BinaryNode<T> curNode) {
         return balance(super.add(value, curNode));
     }
 
     @Override
+    public void remove(T value) {
+        this.root = remove(value, this.root);
+    }
+
     protected BinaryNode<T> remove(T value, BinaryNode<T> curNode) {
         return balance( super.remove(value, curNode));
     }
