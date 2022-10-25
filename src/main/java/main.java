4407fd08bd7e53c454f5980997a7e5079cffa4ec
Elouan Guyon <elouan.guyon@polymtl.ca>
2022-10-24 22:03:36 -0400

    Added Search for base avl and bst

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -16,31 +16,49 @@ public class main {
 
         AvlTree<Integer> avl = new AvlTree<>();
         BinarySearchTree<Integer> bst = new BinarySearchTree<>();
-         
+
         ArrayList<String> csvBstInsertion = new ArrayList<>();
+        ArrayList<String> csvBstSearch = new ArrayList<>();
         writeCsvHeaders(csvBstInsertion);
+        writeCsvHeaders(csvBstSearch);
 
         ArrayList<String> csvAvlInsertion = new ArrayList<>();
+        ArrayList<String> csvAvlSearch = new ArrayList<>();
         writeCsvHeaders(csvAvlInsertion);
+        writeCsvHeaders(csvAvlSearch);
 
-        //Insertion
+        // Insertion
         for (Integer i = 0; i < 100; i++){
-            //Méthode de trouver un nombre random trouvé sur : 
-            //https://stackoverflow.com/questions/363681/how-do-i-generate-random-integers-within-a-specific-range-in-java
+            // Méthode de trouver un nombre random trouvé sur :
+            // https://stackoverflow.com/questions/363681/how-do-i-generate-random-integers-within-a-specific-range-in-java
             Integer value = ThreadLocalRandom.current().nextInt(MIN, MAX + 1);
             insertedValues.add(value);
             computeInsertionStats(value, bst, csvBstInsertion, i);
             computeInsertionStats(value, avl, csvAvlInsertion, i);
         }
 
-        //Write to file for plotting graphs
+        bst.counter.resetCounter();
+        avl.counter.resetCounter();
+
+        // Search
+        Integer index = 1;
+        for (Integer value : insertedValues) {
+            computeSearchStats(value, bst, csvBstSearch, index);
+            computeSearchStats(value, avl, csvAvlSearch, index);
+            index++;
+        }
+
+        // Write to file for plotting graphs
         writeToFile(new File("bstInsertion.csv"), csvBstInsertion);
         writeToFile(new File("avlInsertion.csv"), csvAvlInsertion);
+        writeToFile(new File("bstSearch.csv"), csvBstInsertion);
+        writeToFile(new File("avlSearch.csv"), csvAvlSearch);
     }
 
     private static void writeToFile(File file, ArrayList<String> csv) throws IOException{
+        file.createNewFile();
         if (file.exists()){
-            try(FileWriter filewriter = new FileWriter(file);){
+            try(FileWriter filewriter = new FileWriter(file)){
                 for (int i = 0; i < csv.size(); i+=3){
                     filewriter.write(csv.get(i) + ", " + csv.get(i+1) + ", " + csv.get(i+2) + ",\n");
                 }
@@ -58,7 +76,7 @@ public class main {
         
         csv.add(Integer.toString(nodeCounter));
         csv.add(Integer.toString(counter));
-        csv.add(Integer.valueOf((int) time).toString()); //La conversion de long en int est valide puisque les temps d'exécutions sont rapides et les valeurs petites
+        csv.add(Integer.valueOf((int) time).toString()); // La conversion de long en int est valide puisque les temps d'exécutions sont rapides et les valeurs petites
     }
 
     private static void computeSearchStats(Integer value, BinarySearchTree<Integer> tree, ArrayList<String> csv, Integer index){        
@@ -69,7 +87,7 @@ public class main {
         
         csv.add(index.toString());
         csv.add(Integer.toString(counter));
-        csv.add(Integer.valueOf((int) time).toString()); //La conversion de long en int est valide puisque les temps d'exécutions sont rapides et les valeurs petites
+        csv.add(Integer.valueOf((int) time).toString()); // La conversion de long en int est valide puisque les temps d'exécutions sont rapides et les valeurs petites
     }
 
     private static void writeCsvHeaders(ArrayList<String> csv){
