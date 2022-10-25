a846bb987a03afef01c9012838402af22140fc99
Elouan Guyon <elouan.guyon@polymtl.ca>
2022-10-24 21:18:16 -0400

    Removed unused code for now. Only focus on insert

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -16,24 +16,14 @@ public class main {
 
         AvlTree<Integer> avl = new AvlTree<>();
         BinarySearchTree<Integer> bst = new BinarySearchTree<>();
-        AvlTree<Integer> worstAvl = new AvlTree<>();
-        BinarySearchTree<Integer> worstBst = new BinarySearchTree<>();
          
         ArrayList<String> csvBstInsertion = new ArrayList<>();
-        /*ArrayList<String> csvBstSearch = new ArrayList<>();
-        ArrayList<String> csvBstInsertionWorst = new ArrayList<>();*/
 
         writeCsvHeaders(csvBstInsertion);
-        /*writeCsvHeaders(csvBstSearch);
-        writeCsvHeaders(csvBstInsertionWorst);*/
 
         ArrayList<String> csvAvlInsertion = new ArrayList<>();
-        /*ArrayList<String> csvAvlSearch = new ArrayList<>();
-        ArrayList<String> csvAvlInsertionWorst = new ArrayList<>();*/
 
         writeCsvHeaders(csvAvlInsertion);
-        /*writeCsvHeaders(csvAvlSearch);
-        writeCsvHeaders(csvAvlInsertionWorst);*/
 
         //Insertion
         for (Integer i = 0; i < 100; i++){
@@ -43,37 +33,11 @@ public class main {
             insertedValues.add(value);
             computeInsertionStats(value, bst, csvBstInsertion, i);
             computeInsertionStats(value, avl, csvAvlInsertion, i);
-            //computeInsertionStats(i, bst, csvBstInsertionWorst, i);
         }
-        
-        //Search
-        Integer index = 1;
-        /*for (Integer value : insertedValues) {
-            computeSearchStats(value, bst, csvBstSearch, index);
-            computeSearchStats(value, avl, csvAvlSearch, index++);
-        }*/
 
         //Write to file for plotting graphs
-        File fileBstInsertion = new File("bstInsertion.csv");
-        //fileBstInsertion.createNewFile();
-
-        File fileAvlInsertion = new File("avlInsertion.csv");
-        //fileAvlInsertion.createNewFile();
-
-        File fileBstSearch = new File("bstSearch.csv");
-        //fileBstSearch.createNewFile();
-
-        File fileAvlSearch = new File("avlSearch.csv");
-        //fileAvlSearch.createNewFile();
-
-        File fileBstInsertionWorst = new File("bstInsertionWorst.csv");
-        //fileBstInsertionWorst.createNewFile();
-
-        writeToFile(fileBstInsertion, csvBstInsertion);
-        writeToFile(fileAvlInsertion, csvAvlInsertion);
-        /*writeToFile(fileBstSearch, csvBstSearch);
-        writeToFile(fileAvlSearch, csvAvlSearch);
-        writeToFile(fileBstInsertionWorst, csvBstInsertionWorst);*/
+        writeToFile(new File("bstInsertion.csv"), csvBstInsertion);
+        writeToFile(new File("avlInsertion.csv"), csvAvlInsertion);
     }
 
     private static void writeToFile(File file, ArrayList<String> csv) throws IOException{
@@ -96,7 +60,6 @@ public class main {
         int nodeCounter = index + 1;
         
         TIMER.timerInit();
-        //tree.counter.resetCounter();
         tree.add(insertedValue);
         int counter = tree.counter.getCounter();
         long time = TIMER.getTimerValue();
@@ -108,7 +71,6 @@ public class main {
 
     private static void computeSearchStats(Integer value, BinarySearchTree<Integer> tree, ArrayList<String> csv, Integer index){        
         TIMER.timerInit();
-        //tree.counter.resetCounter();
         tree.contains(value);
         int counter = tree.counter.getCounter();
         long time = TIMER.getTimerValue();
